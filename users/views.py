fbdd01a9be38ea41ba70846c3d5d711ef31293e1
jay <jaewhan232@naver.com>
2023-01-27 11:56:21 +0900

    Feat  : User 레코드 생성시, 인증 이메일 전송 기능 구현

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
 from rest_framework import status, views
 from rest_framework.response import Response
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+
+from utils.user_activate_token_generator import user_activation_token
+from utils.custom_emails import UserActivationEmail
 
 from .serializers import *
-
 
 # Create your views here.
 
@@ -11,5 +15,16 @@ class UserListView(views.APIView):
     def post(self, request):
         serializer = UserBaseSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
+        user = serializer.save()
+        
+        uidb64 = urlsafe_base64_encode(force_bytes(user.id))
+        token = user_activation_token.make_token(user)
+        
+        # TODO: 비동기 처리
+        email = UserActivationEmail(uidb64, token, to=[user.email,])
+        email.send()
+        return Response(
+            serializer.data,
+            status = status.HTTP_201_CREATED
+        )
+        
