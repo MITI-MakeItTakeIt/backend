c987640df043c5264f08322eb90aa65afd870e80
jay <jaewhan232@naver.com>
2023-01-30 20:09:03 +0900

    Test : 닉네임 확인 테스트케이스 작성 - 사용 가능한 닉네임

--- users/tests.py
+++ users/tests.py
@@ -32,3 +32,20 @@ class UserInfoCheckAPITest(APITestCase):
         self.assertIn('email', response.json()['data'])
         self.assertEqual("사용 가능한 이메일입니다.", response.json()['data']['email'])
     
+    def test_givenNonexistingUserNickname_whenCheckUserInfo_thenReturn200(self):
+        # given
+        url = reverse('user-nickname-check')
+        data = {
+            "nickname" : "NonExistingNickname",
+        }
+        
+        # when
+        response = self.client.post(url, data=data, format='json')
+        
+        # then
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('status_code', response.json())
+        self.assertIn('message', response.json())
+        self.assertIn('data', response.json())
+        self.assertIn('nickname', response.json()['data'])
+        self.assertEqual("사용 가능한 닉네임입니다.", response.json()['data']['nickname'])
