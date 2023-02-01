ec91ca09281db9377fcc57568d700ae12eb99df9
jay <jaewhan232@naver.com>
2023-02-01 14:18:08 +0900

    Feat : 입력한 사용자 정보의 중복, 유효성을 검증하는 UserInfoCheckView구현 및 api 생성

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from .views import *
 urlpatterns = [
     path('', UserListView.as_view()),
     path('activate/<str:uidb64>/<str:token>/', UserActivateView.as_view()),
+    path('user-info-check/', UserInfoCheckView.as_view(), name="user-info-check"),
 ]
--- users/views.py
+++ users/views.py
@@ -6,6 +6,7 @@ from django.utils.encoding import force_bytes, force_str, DjangoUnicodeDecodeErr
 
 from utils.user_activate_token_generator import user_activation_token
 from utils.custom_emails import UserActivationEmail
+from utils.custom_responses import UserInfoCheckResponse
 
 from .serializers import *
 
@@ -49,4 +50,16 @@ class UserActivateView(views.APIView):
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
+class UserInfoCheckView(views.APIView):
+    
+    def get(self, request):
+        check_field = request.GET.get('key', None)
+        input_value = request.GET.get('value', None)
         
+        response_data = UserInfoCheckResponse({check_field: input_value})
+        response_data.is_valid()
+        
+        return Response(
+            response_data.response,
+            status = status.HTTP_200_OK
+        )
