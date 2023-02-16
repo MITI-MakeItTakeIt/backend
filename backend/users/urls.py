7ebda426449592ded3735ab2852e93c9fb939072
jay <jaewhan232@naver.com>
2023-02-16 17:22:34 +0900

    Feat : CallbackView url 연동

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('user-info-check/', UserInfoCheckView.as_view(), name="user-info-check"),
     path('login/', UserLoginView.as_view(), name="login"),
     path('social-login-url/', SocialLoginView.as_view(), name="social-login-url"),
+    path('callback/kakao/login/', LoginCallbackView.as_view(), name="kakao-login-callback"),
 ]
